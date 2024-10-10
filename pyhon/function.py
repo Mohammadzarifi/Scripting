d6716dec4531c0fb2c40d5f6d27f225b0b480aeb
zarifi <iqbalzarifi@icloud.com>
2024-10-10 01:52:00 +0000

    pyhon

---
+++ pyhon/function.py
@@ -0,0 +1,8 @@
+ef sum(a, b):
+    result = a + b
+    return result
+arg1 = int(input("please enter first number:"))
+arg2 = int(input("please enter second number"))
+result = sum(arg1, arg2)
+print(result)
+
