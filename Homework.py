9055e5b56d32627a3ce8241eea6581ff8cb79316
zarifi <iqbalzarifi@icloud.com>
2024-10-15 00:16:42 +0000

    why is this not working

---
+++ Homework.py
@@ -0,0 +1,2 @@
+echo "the first argument is: $1"
+echo "the second argument is: $2"
--- pyhon/function.py
+++
@@ -1,8 +0,0 @@
-ef sum(a, b):
-    result = a + b
-    return result
-arg1 = int(input("please enter first number:"))
-arg2 = int(input("please enter second number"))
-result = sum(arg1, arg2)
-print(result)
-
